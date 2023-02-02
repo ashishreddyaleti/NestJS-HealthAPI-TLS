a4dfdd54c3823ebb24e06a40efa49a1e67f600e6
Ashish.Aleti <ashishreddy8001@gmail.com>
2023-02-01 20:49:07 -0800

    updated content

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
     getHealth(): string {
-        return 'Hello Ashish this is your nestApp on minikube'
+        return 'Hello Ashish this is your NESTAPP-TLS on minikube'
     }
 }
